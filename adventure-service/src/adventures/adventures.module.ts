2642eee565faeb7ea25aa5eec4ef2fa1e57f0a73
dan1sh70 <danishshafiq458@gmail.com>
2026-08-21 00:39:23 +0530

    fix: resolve unknown dependencies crash in adventure module

--- adventure-service/src/adventures/adventures.module.ts
+++ adventure-service/src/adventures/adventures.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdventuresService } from './adventures.service';
 import { AdventuresController } from './adventures.controller';
 import { TravelAdventure } from './entities/adventure.entity';
+import { AdventureReview } from './entities/adventure-review.entity';
+import { AdventureSlot } from './entities/adventure-slot.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TravelAdventure])],
+  imports: [TypeOrmModule.forFeature([TravelAdventure, AdventureReview, AdventureSlot])],
   controllers: [AdventuresController],
   providers: [AdventuresService],
 })
